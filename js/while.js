f0ae5afbf69384c922e64fbe4db0eee714a9088f
robertocuyar <roberto.cuyar44@gmail.com>
2020-08-18 09:29:33 -0500

    break_and_continue test updates

--- js/while.js
+++ js/while.js
@@ -26,3 +26,4 @@ function areThereEnoughCones () {
 }
 areThereEnoughCones();
 
+
--- tests/break_and_continue_tests.js
+++ tests/break_and_continue_tests.js
@@ -9,3 +9,18 @@ describe("isMyNumberOdd", function() {
         expect(isMyNumberOdd(65)).toBeUndefined();
     });
 });
+
+describe("isMyNumberOddList", function() {
+    it('should be defined', function () {
+        expect(isMyNumberOddList).toBeDefined();
+    });
+    it('should return undefined statement and prompt when given an even number', function(){
+        expect(isMyNumberOddList(2)).toBeUndefined();
+    });
+    it('should return undefined statement and prompt when given a number greater than 50', function() {
+        expect(isMyNumberOddList(65)).toBeUndefined();
+    });
+    it('should return undefined and prompt when given a null', function(){
+        expect(isMyNumberOddList(null)).toBeUndefined();
+    });
+})
