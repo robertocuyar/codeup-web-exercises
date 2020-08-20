22b86e1a9ea15682e155b7c1b14f33bcfa25a4e6
robertocuyar <roberto.cuyar44@gmail.com>
2020-08-20 10:31:00 -0500

    Morning Introduction Array Exercises

--- js/while.js
+++ js/while.js
@@ -1,6 +1,5 @@
 var exponent = 1;
 function exponentialProblem () {
-
     while (exponent <= 16) {
         console.log(Math.pow(2, exponent));
         exponent++;
@@ -9,20 +8,23 @@ function exponentialProblem () {
 exponentialProblem();
 
 var allCones = Math.floor(Math.random() * 50) + 50;
-var soldCones = Math.floor(Math.random() * 5) + 1;
+
 function areThereEnoughCones () {
 
     do {
+        var soldCones = Math.floor(Math.random() * 5) + 1;
         console.log (soldCones + ' cones sold');
-       console.log(allCones = allCones - soldCones);
-    } while (soldCones <= allCones);
-    if (soldCones < allCones) {
-        console.log(soldCones + ' cones sold');
-    } else if (soldCones > allCones) {
-        console.log("Cannot sell you " + soldCones + " cones. I only have " + allCones + " left.");
-    } else {
-        console.log("Yay! I sold them all!");
-    }
+        if (soldCones < allCones) {
+            console.log(soldCones + ' cones sold');
+            console.log(allCones = allCones - soldCones);
+        } else if (soldCones > allCones && allCones !== 0) {
+            console.log("Cannot sell you " + soldCones + " cones. I only have " + allCones + " left.");
+            break;
+        } else if(allCones === 0){
+            console.log("Yay! I sold them all!");
+        }
+    } while (0 < allCones);
+
 }
 areThereEnoughCones();
 
