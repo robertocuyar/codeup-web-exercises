ac4979d98287ddf8e0f330ccf4eccec5130b24cc
robertocuyar <roberto.cuyar44@gmail.com>
2020-09-22 11:38:47 -0500

    small exercise changes

--- js/promises.js
+++ js/promises.js
@@ -7,3 +7,22 @@ let input = document.getElementById('user_input').value
         .then(data => output.innerHTML = data[0].created_at)
         .catch(error => output.innerHTML = `404 Username not found. ${error}`)
 }
+
+const wait = number => {
+    return new Promise((resolve, reject) => {
+        if (!isNaN(parseFloat(number)))
+        setTimeout(()=>{
+            if (number <= 1000)
+                resolve(`You'll see this after ${number/1000} second`)
+            else
+                resolve(`You'll see this after ${number/1000} seconds`)
+        }, number)
+        else
+            reject("Error: value is not a number.")
+
+    })
+}
+wait(6000).then(data => console.log(data))
+wait(1000).then(data => console.log(data))
+wait('500').then(data => console.log(data))
+wait('cheese').then(data => console.log(data))
